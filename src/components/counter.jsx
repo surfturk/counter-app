d5a69f3c35257469940ec8b12ccd531549deee86
Berkley Brown <berkleydeane@gmail.com>
2020-10-28 15:11:45 -0500

    fixed increment issue with not changing state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ["tag1", "tag2", "tag3"],
   };
 
-  handleIncrement = (product) => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -16,7 +15,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement(product)}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -32,12 +31,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import Counter from "./counter";
 
 class Counters extends Component {
-  state = {};
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        {this.state.counters.map((counter) => (
+          <Counter key={counter.id} value={counter.value} />
+        ))}
+      </div>
+    );
   }
 }
 
